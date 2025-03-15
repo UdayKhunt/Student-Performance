2d9f12fcf42e5dac5bc0bb8bd46d3f9c015b5a99
UdayKhunt <udaykhunt02@gmail.com>
2025-03-15 18:55:58 +0530

    DockerFile added

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def predict_datapoint():
         return render_template('form.html' , results = res[0])
 
 if __name__ == '__main__':
-    app.run(debug=True , host = '0.0.0.0')
+    app.run(debug=True , host = '0.0.0.0' , port = 5000)
